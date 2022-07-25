35c1ffbf6a524c549f1370547ddab764aa439bf0
nevin <aydingezernevin@gmail.com>
2022-07-25 19:13:12 +0300

    Further commit

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -19,7 +19,7 @@ import org.junit.runner.RunWith;
         glue = "com/cydeo/step_definitions",
         dryRun = false,
         publish = false,
-        tags = "@nev"
+        tags = ""
 )
 public class CukesRunner {
 }
